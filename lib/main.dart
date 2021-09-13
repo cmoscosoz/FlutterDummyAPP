a21a760224c964291cdb7dc5b37cdd1c1ac7e92a
Claudio Moscoso <cmoscosoz@gmail.com>
2021-09-14 11:21:42 +1200

    dummy flutter app

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+import './quiz.dart';
+import './result.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questions = [
+    {
+      'questionText': 'What\'s your favourite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1}
+      ]
+    },
+    {
+      'questionText': 'What\'s your favourite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 10},
+        {'text': 'Snake', 'score': 5},
+        {'text': 'Elephant', 'score': 20},
+        {'text': 'Lion', 'score': 0}
+      ]
+    },
+    {
+      'questionText': 'Who\'s your favourite person?',
+      'answers': [
+        {'text': 'Hannah', 'score': 1},
+        {'text': 'Teo', 'score': 1},
+        {'text': 'Hannah', 'score': 1},
+        {'text': 'Teo', 'score': 1}
+      ]
+    },
+  ];
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _restQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+      _totalScore = _totalScore + score;
+      print(_totalScore);
+    });
+    if (_questionIndex < _questions.length) {
+      print('we have more questions!');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First APP'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _restQuiz),
+      ),
+    );
+  }
+}
